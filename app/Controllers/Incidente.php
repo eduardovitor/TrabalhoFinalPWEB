9cd55a056cf89c4cc3c43375950277ef49d298f4
Tamilly Nascimento <67803749+tamilly@users.noreply.github.com>
2021-09-14 13:27:44 -0300

    Update Incidente.php

--- app/Controllers/Incidente.php
+++ app/Controllers/Incidente.php
@@ -4,8 +4,22 @@ namespace App\Controllers;
 
 class Incidente extends BaseController
 {
+    use ResponseTrait;
+    
     public function index()
     {
         return view('incidente');
+    }
+    
+    public function cadastrar()
+    {
+        $incidente_model = new \App\Models\IncidenteModel();
+        $incidente =$incidente_model->save($this->request->getPost());
+        return view('home');
+    }
+
+    public function visualizar()
+    {
+        
     }
 }
