45ec1daac545ee1da88717a20eab7d17b75b7948
Tamilly Nascimento <67803749+tamilly@users.noreply.github.com>
2021-09-17 14:28:07 -0300

    Update Incidente.php

--- app/Controllers/Incidente.php
+++ app/Controllers/Incidente.php
@@ -18,8 +18,51 @@ class Incidente extends BaseController
         return view('home');
     }
 
-    public function visualizar()
+    public function visualizarPorTipo()
     {
-        
+        $db = \Config\Database::connect();
+        $sql = "SELECT * FROM incidente ORDER BY tipo";
+        $resultado = $db->query($sql);
+        $linhas_afetadas = $resultado->getNumRows()
+        if($linhas_afetadas > 0){
+            while($row = $resultado->fetch_assoc()){
+                echo "ID: " . $row["id"] . "Tipo: " . $row["tipo"] . "Nome: " . $row["nome"] . "Localização: " . $row["localizacao"] . "Data de cadastro: " . $row["data_cadastro"] . "Foto: " . $row["path_foto"] . "Condição do incidente: " . $row["condicao_incidente"] . "<br>";
+            }
+        }
+        else{
+            echo "Não há incidentes cadastrados!";
+        }
+    }
+
+    public function visualizarPorLocalizacao()
+    {
+        $db = \Config\Database::connect();
+        $sql = "SELECT * FROM incidente ORDER BY localizacao";
+        $resultado = $db->query($sql);
+        $linhas_afetadas = $resultado->getNumRows()
+        if($linhas_afetadas > 0){
+            while($row = $resultado->fetch_assoc()){
+                echo "ID: " . $row["id"] . "Tipo: " . $row["tipo"] . "Nome: " . $row["nome"] . "Localização: " . $row["localizacao"] . "Data de cadastro: " . $row["data_cadastro"] . "Foto: " . $row["path_foto"] . "Condição do incidente: " . $row["condicao_incidente"] . "<br>";
+            }
+        }
+        else{
+            echo "Não há incidentes cadastrados!";
+        }
+    }
+
+    public function visualizarPorData()
+    {
+        $db = \Config\Database::connect();
+        $sql = "SELECT * FROM incidente ORDER BY data_cadastro";
+        $resultado = $db->query($sql);
+        $linhas_afetadas = $resultado->getNumRows()
+        if($linhas_afetadas > 0){
+            while($row = $resultado->fetch_assoc()){
+                echo "ID: " . $row["id"] . "Tipo: " . $row["tipo"] . "Nome: " . $row["nome"] . "Localização: " . $row["localizacao"] . "Data de cadastro: " . $row["data_cadastro"] . "Foto: " . $row["path_foto"] . "Condição do incidente: " . $row["condicao_incidente"] . "<br>";
+            }
+        }
+        else{
+            echo "Não há incidentes cadastrados!";
+        }
     }
 }
